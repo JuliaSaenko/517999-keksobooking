241c40e1d396fad7bfd36af6a12cfd8323aa6f5d
JuliaSaenko <julia.saenko94@gmail.com>
2018-12-13 12:27:27 +0200

    Разделяет код на модули

---
+++ js/card.js
@@ -0,0 +1,78 @@
+'use strict';
+
+(function () {
+
+  var map = document.querySelector('.map');
+  var mapCardTemplate = document.querySelector('template').content.querySelector('article.map__card');
+  var cardTemplate = document.querySelector('#card').content;
+
+  var PlaceType = {
+    palace: 'Дворец',
+    flat: 'Квартира',
+    house: 'Дом',
+    bungalo: 'Бунгало'
+  };
+
+  var renderFeatures = function (features) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < features.length; i++) {
+      var featuresItem = document.createElement('li');
+      featuresItem.className = 'popup__feature popup__feature--' + features[i];
+      fragment.appendChild(featuresItem);
+    }
+    return fragment;
+  };
+
+  var renderPhoto = function (card) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < card.offer.photos.length; i++) {
+      var createImg = cardTemplate.querySelector('.map__card')
+      .querySelector('.popup__photo').cloneNode(true);
+
+      createImg.src = card.offer.photos[i];
+      fragment.appendChild(createImg);
+    }
+    return fragment;
+  };
+
+  var onPopupEscPress = function (evt) {
+    window.util.isEscEvent(evt, closePopup);
+  };
+
+  var closePopup = function () {
+    var noticeCard = map.querySelector('.map__card.popup');
+    if (noticeCard) {
+      map.removeChild(noticeCard);
+      document.removeEventListener('keydown', onPopupEscPress);
+    }
+  };
+
+  var renderCards = function (card) {
+    var cardElement = mapCardTemplate.cloneNode(true);
+    var featuresItems = cardElement.querySelector('.popup__features');
+    var photosItems = cardElement.querySelector('.popup__photos');
+
+    cardElement.querySelector('.popup__title').textContent = card.offer.title;
+    cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+    cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
+    cardElement.querySelector('.popup__type').textContent = PlaceType[card.offer.type];
+    cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
+    cardElement.querySelector('.popup__text--time').textContent = 'Заезд после' + card.offer.checkin + ' выезд до ' + card.offer.checkout;
+    cardElement.querySelector('.popup__description').textContent = card.offer.description;
+    photosItems.innerHTML = '';
+    cardElement.querySelector('.popup__photos').appendChild(renderPhoto(card));
+    cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+
+    featuresItems.textContent = '';
+    featuresItems.appendChild(renderFeatures(card.offer.features));
+
+    return cardElement;
+  };
+
+  window.card = {
+    onPopupEscPress: onPopupEscPress,
+    closePopup: closePopup,
+    renderCards: renderCards
+  };
+})();
---
+++ js/util.js
@@ -0,0 +1,48 @@
+'use strict';
+
+(function () {
+
+  var ESC_KEYCODE = 27;
+
+  var shuffleArray = function (array) {
+    var clonedArray = array.slice();
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return clonedArray;
+  };
+
+  var getRandomIntegerFromInterval = function (min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+  };
+
+  var getRandomElementFromArray = function (array) {
+    return array[getRandomIntegerFromInterval(0, array.length - 1)];
+  };
+
+  var getRandomNumber = function (from, to) {
+    return Math.floor(Math.random() * (to - from + 1) + from);
+  };
+
+  var getRandomSubarray = function (array) {
+    shuffleArray(array);
+    return array.slice(getRandomNumber(1, array.length - 1));
+  };
+
+  var isEscEvent = function (evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
+    }
+  };
+
+  window.util = {
+    getRandomIntegerFromInterval: getRandomIntegerFromInterval,
+    getRandomElementFromArray: getRandomElementFromArray,
+    shuffleArray: shuffleArray,
+    getRandomSubarray: getRandomSubarray,
+    isEscEvent: isEscEvent
+  };
+})();
