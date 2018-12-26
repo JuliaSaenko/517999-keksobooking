051ccf08e498fa66fd1f51fdeb7aa819b2958cc3
JuliaSaenko <julia.saenko94@gmail.com>
2018-12-26 11:43:19 +0200

    Правки по критерию Д21

--- js/filters.js
+++ js/filters.js
@@ -7,48 +7,56 @@
   var filtersSelect = filtersForm.querySelectorAll('select');
   var filtersFieldsets = filtersForm.querySelectorAll('fieldset');
 
+  var filterType = filtersForm.querySelector('#housing-type');
+  var filterPrice = filtersForm.querySelector('#housing-price');
+  var filterRooms = filtersForm.querySelector('#housing-rooms');
+  var filterGuests = filtersForm.querySelector('#housing-guests');
+  var filterFeatures = filtersForm.querySelector('#housing-features').querySelectorAll('input[name=features');
+
   var getFiltredPins = function (data) {
     var arrayCopy = data.slice();
 
-    var filterType = filtersForm.querySelector('#housing-type').value;
-    var filterPrice = filtersForm.querySelector('#housing-price').value;
-    var filterRooms = filtersForm.querySelector('#housing-rooms').value;
-    var filterGuests = filtersForm.querySelector('#housing-guests').value;
-    var filterFeatures = filtersForm.querySelector('#housing-features').querySelectorAll('input[name=features]:checked');
+    var typeValue = filterType.value;
+    var priceValue = filterPrice.value;
+    var roomsValue = filterRooms.value;
+    var guestsValue = filterGuests.value;
 
-    var featuresValue = [];
+    var checkedFeatures = [];
     for (var i = 0; i < filterFeatures.length; i++) {
-      featuresValue[i] = filterFeatures[i].value;
+      if (filterFeatures[i].checked) {
+        checkedFeatures.push(filterFeatures[i].value);
+      }
     }
 
     var filtredNotices = arrayCopy.filter(function (notice) {
       var isTrue = true;
-      if (filterType !== START_WEIGHT && notice.offer.type !== filterType) {
+      if (typeValue !== START_WEIGHT && notice.offer.type !== typeValue) {
         isTrue = false;
       }
-      if (filterPrice === 'low' && notice.offer.price > 10000) {
+      if (priceValue === 'low' && notice.offer.price > 10000) {
         isTrue = false;
-      } else if (filterPrice === 'middle' && (notice.offer.price > 50000 || notice.offer.price < 10000)) {
+      } else if (priceValue === 'middle' && (notice.offer.price > 50000 || notice.offer.price < 10000)) {
         isTrue = false;
-      } else if (filterPrice === 'high' && notice.offer.price < 50000) {
-        isTrue = false;
-      }
-      if (filterRooms !== START_WEIGHT && notice.offer.rooms.toString() !== filterRooms) {
+      } else if (priceValue === 'high' && notice.offer.price < 50000) {
         isTrue = false;
       }
-      if (filterGuests !== START_WEIGHT && notice.offer.guests.toString() !== filterGuests) {
+      if (roomsValue !== START_WEIGHT && notice.offer.rooms.toString() !== roomsValue) {
         isTrue = false;
       }
-      for (i = 0; i < featuresValue.length; i++) {
+      if (guestsValue !== START_WEIGHT && notice.offer.guests.toString() !== guestsValue) {
+        isTrue = false;
+      }
+      for (i = 0; i < checkedFeatures.length; i++) {
         if (!isTrue) {
           break;
         }
-        isTrue = notice.offer.features.includes(document.querySelector('#filter-' + featuresValue[i]).value);
+        isTrue = notice.offer.features.includes(document.querySelector('#filter-' + checkedFeatures[i]).value);
       }
       return isTrue;
     });
     return filtredNotices;
   };
+
 
   var disableFilters = function () {
     for (var i = 0; i < filtersSelect.length; i++) {
